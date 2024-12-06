f0c7db36b7ca72aa8a59aaca0f6fcddf07ec6743
ivan.sapeha <ivan.sapeha@lseg.com>
2024-12-06 18:58:11 +0200

    Added versioning

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { SheetGenerator } from '@components/markup/SheetGenerator';
 
 export const App = () => {
-    // return <SpellCards spells={spells} />;
     return (
         <div
             className={
-                'w-full flex items-center flex-col pb-[30px] small:items-start small:pr-[2mm] small:pl-[2mm]'
+                'w-full flex items-center flex-col pb-[30px] small:items-start small:pr-[2mm] small:pl-[2mm] gap-[5mm]'
             }
         >
             <SheetGenerator />
+            <footer className='text-white flex gap-[5mm]'>
+                <span>Version 1.2</span>
+                <span>Author: Ivan Sapeha</span>
+            </footer>
         </div>
     );
 };
