b621644e69893c5e57c1d11da7b9bd2c57703d02
ivan.sapeha <ivan.sapeha@lseg.com>
2024-12-11 13:34:58 +0200

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/character-sheet/sw.js', { scope: '/character-sheet/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
